dd63ca2ea46e6e1fb38ad58d1fc79ac4c2b75d3a
Kym Reilly <kymreilly@hotmail.com>
2022-03-16 18:04:52 +1030

    commented html, js. Added links to 'Projects'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,25 @@
+
+// Declare and assign the variables
 const readMoreBtn = document.querySelector('.read-more-btn');
 const text = document.querySelector('.text');
 
+
+/* 
+Web application waiting for an trigger, specifically a 'click, to run the code'
+When the specified Button in the web app is clicked, the class selector of 'Text' toggles
+between 'text' and 'text show-more'. This enables the previously hidden text to appear.
+*/
 readMoreBtn.addEventListener('click',(e)=>{
     text.classList.toggle('show-more');
 })
 
 
+
+/*
+Again, waiting for a 'click' to to run the code
+When the specified Button in the web app is clicked, the page scrolls 
+to the preassigned destination with the scroll option being smooth and not light-speed
+*/
 document.querySelector('.read-more-btn').addEventListener('click', function() {
 
   document.querySelector('#more-about').scrollIntoView({
